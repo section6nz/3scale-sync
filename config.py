f56437bf36f633d10563f915d7e69843c04652a6
Frank Zhao <frank@frankzhao.net>
2021-08-24 12:32:48 +1000

    Check for dupes only if there are more then one

--- config.py
+++ config.py
@@ -59,7 +59,7 @@ class Config:
         err_reason = "This will lead to system name conflicts. Please resolve this before continuing."
         # Ensure product system names are unique.
         system_names = [p.shortName for p in self.products]
-        if len(system_names) != len(set(system_names)):
+        if len(system_names) > 1 and len(system_names) != len(set(system_names)):
             raise AssertionError("ABORT: Product short names are not unique. " + err_reason)
 
         backend_names = []
@@ -69,17 +69,17 @@ class Config:
             application_names.extend([a.name for a in product.applications])
 
         # Ensure backend names are unique.
-        if len(backend_names) != len(set(backend_names)):
+        if len(backend_names) > 1 and len(backend_names) != len(set(backend_names)):
             raise AssertionError("ABORT: Backend ids are not unique. " + err_reason)
 
         # Ensure application names are unique.
-        if len(application_names) != len(set(application_names)):
+        if len(application_names) > 1 and len(application_names) != len(set(application_names)):
             raise AssertionError("ABORT: Application names are not unique. " + err_reason)
 
         # Ensure backend paths are not duplicated.
         for product in self.products:
             paths = [b.path for b in product.backends]
-            if len(paths) != len(set(paths)):
+            if len(paths) > 1 and len(paths) != len(set(paths)):
                 raise AssertionError("ABORT: Backend paths are not unique. "
                                      "Please resolve before continuing. product={}".format(product.name))
 
